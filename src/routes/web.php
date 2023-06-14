18f888244a69f386c11541725a83885068858817
関目智史 <zou3road@gmail.com>
2023-06-14 23:28:25 +0900

    ルーティングを追加

--- src/routes/web.php
+++ src/routes/web.php
@@ -10,7 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 下の書き方は、Laravel5.2以前の書き方。functionは、Laravel5.3以降は使えない。
 Route::get('/', function () {
     return view('welcome');
 });
+// 以下、追加。Laravel5.3以降の書き方。
+Route::get('todos', 'TodosController@index');
+Route::get('todos/create', 'TodosController@create');
+Route::post('todos', 'TodosController@store');
+Route::get('todos/{id}', 'TodosController@show');
+Route::get('todos/{id}/edit', 'TodosController@edit');
+Route::put('todos/{id}', 'TodosController@update');
+Route::delete('todos/{id}', 'TodosController@destroy');
