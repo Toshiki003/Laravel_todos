1f5309a826d813953e1750bbfdb7e92c1ab63dd9
関目智史 <zou3road@gmail.com>
2023-06-15 00:54:47 +0900

    Add:Todo一覧ページのビューを実装

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo; // todoモデルを使うために追加
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
@@ -13,7 +14,9 @@ class TodoController extends Controller
      */
     public function index()
     {
-        //
+        $todos = Todo::all(); // todoモデルの全てのレコードを取得
+
+        return view(('todo/index'), compact('todos')); // view関数の第二引数にcompact関数を使うことで、第一引数で指定した変数をviewに渡すことができる。
     }
 
     /**
